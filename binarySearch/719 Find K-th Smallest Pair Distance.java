4bb728a12df15a0d8b92a3ef4c95300020c3a459
Jayash Kuchia <jayashkuchia@gmail.com>
2020-09-21 18:23:29 +0530

    Create 719 Find K-th Smallest Pair Distance.java

---
+++ binarySearch/719 Find K-th Smallest Pair Distance.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int smallestDistancePair(int[] nums, int k) {
+        if(nums.length <= 1 || k==0) return -1;
+
+        Arrays.sort(nums);
+        
+        int left =  0;
+        int right = nums[nums.length-1] - nums[0];
+        
+        while(left < right) {
+            int mid = left + (right - left)/2;
+            if(getCount(mid, nums) < k) left = mid + 1;
+            else right = mid;            
+        }
+        
+        return left;
+    }
+    
+    public int getCount(int mid, int[] nums) {
+        
+        int left = 0;
+        int count = 0;
+        int right = 1;
+        
+        while(right < nums.length) {
+            
+            if(nums[right] - nums[left] <= mid) {
+                if(right == nums.length -1) {
+                    count += right - left;
+                    left++;
+                } else count += right - left;
+                right++;
+            } else left++;
+        }
+        
+        return count;
+    }
+}
+
+
+
+/*
+ 2
+ 5
+ 0 1 2 3 4 5 6
+ 1 1 3 4 6 7 8
+         |   |
+    
+
+
+*/

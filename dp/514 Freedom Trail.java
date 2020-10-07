a5011f14fdd9b0fef7ad14427cb04532ec13b3e2
Jayash Kuchia <jayashkuchia@gmail.com>
2020-10-07 17:30:02 +0530

    Update 514 Freedom Trail.java

--- dp/514 Freedom Trail.java
+++ dp/514 Freedom Trail.java
@@ -39,3 +39,60 @@ class Solution {
         return ans;
     }
 }
+
+
+/***********************************************************************************************************/
+// using TreeSet
+
+class Solution {
+    public int findRotateSteps(String ring, String key) {
+        if(key == null || key.length() == 0) return 0;
+        
+        Map<Character, TreeSet<Integer>> map = new HashMap<>();
+        
+        for(int i=0; i< ring.length(); i++) {
+            if(map.containsKey(ring.charAt(i))) {
+                map.get(ring.charAt(i)).add(i);
+            } else {
+                TreeSet<Integer> set = new TreeSet<>();
+                set.add(i);
+                map.put(ring.charAt(i), set);
+            }
+        }
+        
+        return solve(key, 0, map, 0, ring, new int[key.length()][ring.length()]) + key.length();
+    }
+    
+    public int solve(String key, int i, Map<Character, TreeSet<Integer>> map,
+                     int curr, String ring, int[][] memo) {
+        if(i == key.length()) return 0;
+        
+        if(memo[i][curr] != 0) return memo[i][curr];
+
+        TreeSet<Integer> set = map.get(key.charAt(i));
+        int appendForLeft = 0;
+        int appendForRight = 0;
+        Integer goLeft = set.floor(curr);
+        if(goLeft == null) {
+            goLeft = set.last();
+            appendForLeft = curr + ring.length() - goLeft;
+        } else appendForLeft = curr - goLeft;
+        Integer goRight = set.ceiling(curr);
+        if(goRight == null) {
+            goRight = set.first();
+            appendForRight = goRight + ring.length() - curr;
+        } else appendForRight = goRight - curr;
+        
+        int ans = 0;
+        if(goLeft == goRight) {
+            ans =  appendForRight > appendForLeft ? appendForLeft + solve(key, i+1, map, goLeft, ring, memo) : 
+                        appendForRight + solve(key, i+1, map, goRight, ring, memo);
+        } else {
+           ans = Math.min(appendForLeft + solve(key, i+1, map, goLeft, ring, memo), 
+                         appendForRight + solve(key, i+1, map, goRight, ring, memo)); 
+        }
+        
+        memo[i][curr] = ans;
+        return ans;
+    }
+}

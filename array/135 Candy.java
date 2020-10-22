5f89e5f4585e270ecb11efc8ee60faa7a09efb67
Jayash Kuchia <jayashkuchia@gmail.com>
2020-10-22 12:16:23 +0530

    Update 135 Candy.java

--- array/135 Candy.java
+++ array/135 Candy.java
@@ -1,9 +1,7 @@
 class Solution {
     public int candy(int[] ratings) {
         if(ratings.length < 2) return ratings.length;
-        
         int candy[] = new int[ratings.length];
-        
         if(ratings[1] >= ratings[0]) candy[0] = 1;
         
         for(int i=1; i< ratings.length - 1; i++) {
@@ -14,16 +12,13 @@ class Solution {
         if(ratings[ratings.length-2] >= ratings[ratings.length -1]) candy[ratings.length - 1] = 1;
         else candy[ratings.length - 1] = candy[ratings.length -2] + 1;
         
+        int ans = candy[candy.length - 1];
         for(int i=ratings.length-2; i>0; i--) {
             if(ratings[i] > ratings[i+1]) candy[i] = Math.max(candy[i], candy[i+1] + 1);
+            ans += candy[i];
         }
-        
         if(ratings[1] < ratings[0]) candy[0] = candy[1] + 1;
-        
-        int ans = 0;
-        
-        for(int i=0; i < candy.length; i++) ans += candy[i];
-        
+        ans += candy[0];
         return ans;
     }
 }
